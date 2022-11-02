465c221e036bab27a6364d7983a883eeb44bc3b1
SOUnit <workbookwork7@gmail.com>
2022-11-01 20:24:33 -0700

    add trigger crash-effect

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -8,10 +8,14 @@ public class CrashDetector : MonoBehaviour
     [SerializeField]
     float delayTime = 0.5f;
 
+    [SerializeField]
+    ParticleSystem crashEffect;
+
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Ground")
         {
+            crashEffect.Play();
             Invoke("ReloadScene", delayTime);
         }
     }
