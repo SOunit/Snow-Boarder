0fcaa718c569a2af1538c1771596157c4c5636bb
SOUnit <workbookwork7@gmail.com>
2022-11-01 21:56:09 -0700

    add boost up logic

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,16 +7,43 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     float torqueAmount = 1f;
 
+    [SerializeField]
+    float boostSpeed = 30f;
+
+    [SerializeField]
+    float baseSpeed = 20f;
+
     Rigidbody2D rb2d;
+
+    SurfaceEffector2D surfaceEffector2D;
 
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        surfaceEffector2D = FindObjectOfType<SurfaceEffector2D>();
     }
 
     // Update is called once per frame
     void Update()
+    {
+        RotatePlayer();
+        RespondToBoost();
+    }
+
+    void RespondToBoost()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            surfaceEffector2D.speed = boostSpeed;
+        }
+        else
+        {
+            surfaceEffector2D.speed = baseSpeed;
+        }
+    }
+
+    void RotatePlayer()
     {
         if (Input.GetKey(KeyCode.LeftArrow))
         {
